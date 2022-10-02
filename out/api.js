7225d526dbaead2403dddbaaadbb2ee2316605b8
EliteMandy <manusingh9812210329@gmail.com>
2022-10-02 20:55:45 +0530

    KMAP

---
+++ out/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "KMap"
+    ],
+    "modules": [
+        "KMapGenerator"
+    ],
+    "allModules": [
+        {
+            "displayName": "KMapGenerator",
+            "name": "KMapGenerator",
+            "description": "Karnaugh Map Generator.\n\nBy Antonio García Díaz.\n\nUniversité Libre de Bruxelles 2016-2017.\n\nThis module was created as a javascript web application to automatically generate K-Map exercices and their solutions.\nIt uses a method (pseudo-ESPRESSO) based on the ESPRESSO algorithm to calculate an optimal cover of the K-Map using n-cubes, and display the solution linked to said covering.\nThe application is meant to be used in conjunction with KarnaughMap.html, which provides an HTML layout to showcase the application."
+        }
+    ],
+    "elements": []
+} };
+});
